e5dd168ffd66ed12245885a87b160a6593da82d9
Coorarra <matveev.m@kompstar.com>
2019-02-26 16:08:38 +0700

    Start repository
    Start repository for create the Game

--- CSharp_Snake_Game/CSharp_Snake_Game/Program.cs
+++ CSharp_Snake_Game/CSharp_Snake_Game/Program.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SnakeGame1
+namespace CSharp_Snake_Game
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            Console.WriteLine("This is a test repository commit");
             Console.ReadLine();
         }
     }
